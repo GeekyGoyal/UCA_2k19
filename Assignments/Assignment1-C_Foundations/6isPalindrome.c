95e84955e50a851f089754ca060103eb0528d8b9
Aparna Goyal <aparnagoyal.0003@gmail.com>
2021-07-25 03:33:10 +0530

    Create 6isPalindrome.c

---
+++ Assignments/Assignment1-C_Foundations/6isPalindrome.c
@@ -0,0 +1,41 @@
+/**  C program to check if the given string is palindrome or not.
+* 
+*   @Aparna Goyal, 1911981092, 24/07/2021  */
+
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+#include <stdbool.h>
+#define N 100
+
+bool isPalindrome(char * str){
+    int len = strlen(str);
+    int low = 0, high = len - 1;
+    
+    while(low < high){
+        if(isalnum(str[low]) && isalnum(str[high])) {
+            if(tolower(str[low]) != tolower(str[high]))
+                return false;
+            low++;
+            high--;
+        }
+        if(!isalnum(str[low]))
+            low++;
+        if(!isalnum(str[high]))
+            high--;        
+    }
+    return true;
+}
+
+int main() {
+    char str[N];
+    printf("Enter the string:\n");
+    fgets(str, N, stdin);
+    if(isPalindrome(str)) {
+        printf("true");
+    }
+    else 
+    printf("false");
+
+    return 0;
+}
