ee97a3da8b3970beb24cf22c7cabec1ba5f5a11a
Aparna Goyal <aparnagoyal.0003@gmail.com>
2021-07-30 05:24:54 +0530

    Update 10squeeze.c

--- Assignments/Assignment1-C_Foundations/10squeeze.c
+++ Assignments/Assignment1-C_Foundations/10squeeze.c
@@ -5,14 +5,11 @@
 #include <stdio.h>
 
 void squeeze(char s1[], char s2[]) {
-    int i, j, m;
-	for(m = 0; s2[m] != '\0'; m++)
-	{
-		for(i = j = 0; s1[i] != '\0'; i++)
-		{
-			if(s1[i] != s2[m])
+    int i, j, k;
+	for (k = 0; s2[k] != '\0'; k++) {
+		for (i = j = 0; s1[i] != '\0'; i++)
+			if (s1[i] != s2[k])
 				s1[j++] = s1[i];
-		}
 		s1[j] = '\0';
 	}
 }
