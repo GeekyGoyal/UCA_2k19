1750cbc14fc4c10eb1f70f3eac29b1d7bcf10e2e
Aparna Goyal <aparnagoyal.0003@gmail.com>
2021-07-30 07:00:00 +0530

    Create 14strLib.c

---
+++ Assignments/Assignment1-C_Foundations/14strLib.c
@@ -0,0 +1,11 @@
+/**  C program to write library functions strncpy, strncat, and strncmp, which operate on at most the first n 
+* characters of their argument strings. For example, strncpy (char *s, char*  t , size_t n) copies at most n 
+* characters of t to s.
+* 
+*   @Aparna Goyal, 1911981092, 29/07/2021  */
+
+#include <stdio.h>
+
+int main() {
+    
+}
