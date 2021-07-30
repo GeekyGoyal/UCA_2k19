774662ea5ec022c945001e6edb169df06d3cdc9e
Aparna Goyal <aparnagoyal.0003@gmail.com>
2021-07-30 07:08:01 +0530

    Create 15evaluate.c

---
+++ Assignment1-C_Foundations/15evaluate.c
@@ -0,0 +1,10 @@
+/**  C program to write expr, which evaluates a reverse Polish expression from the command line, where each operator
+*  or operand is a separate argument. For example, expr 2 3 4 + * evaluates 2 * (3 + 4).
+* 
+*   @Aparna Goyal, 1911981092, 29/07/2021  */
+
+#include <stdio.h>
+
+int main() {
+    
+}
