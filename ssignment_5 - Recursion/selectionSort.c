66933f9ca13d1cb56288b2dd71bba91e8e6f1226
Aparna Goyal <aparnagoyal.0003@gmail.com>
2021-08-04 00:09:13 +0530

    Update selectionSort.c

--- ssignment_5 - Recursion/selectionSort.c
+++ ssignment_5 - Recursion/selectionSort.c
@@ -1,1 +1,44 @@
+/** C program to implement selection sort using recursion.
+* 
+*   @Aparna Goyal, 1911981092, 03/08/2021  */
 
+#include <stdio.h>
+
+void selectionSort(int arr[], int n) {
+
+    //Base Case
+    if (n == 1){ 
+        return;
+    }
+
+    int index = 0;
+
+    for (int i = 1; i < n; i++) {
+        if(arr[i] > arr[index])
+            index = i;
+    }
+
+    int temp = arr[index];
+    arr[index] = arr[n-1];
+    arr[n-1] = temp;
+
+    selectionSort(arr, n - 1);
+
+}
+
+int main() {
+    int n;
+    printf("Enter the size of the array: ");
+    scanf("%d", &n);
+    int arr[n];
+    printf("Enter the elements of the array: ");
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    selectionSort(arr, n);
+    printf("The Sorted array is: ");
+    for(int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    return 0;
+}
