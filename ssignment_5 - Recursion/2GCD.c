8b6f1dce3c3b38079187c5f33605cdb611b81a6e
Aparna Goyal <aparnagoyal.0003@gmail.com>
2021-08-04 00:18:22 +0530

    added

---
+++ ssignment_5 - Recursion/2GCD.c
@@ -0,0 +1,18 @@
+/** C program to find GCD using Recursion.
+* 
+*   @Aparna Goyal, 1911981092, 03/08/2021  */
+
+#include <stdio.h>
+
+void gcd(int n1, int n2) {
+
+}
+
+int main() {
+    int n1, n2;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &n1, &n2);
+    gcd(n1, n2);
+
+    return 0;
+}
