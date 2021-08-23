ce71a76ba75986748b1d21d53cfd6c9745e80536
Aparna Goyal <aparnagoyal.0003@gmail.com>
2021-08-23 15:32:03 +0530

    added Bits and Bytes

--- Assignment3-Bits and Bytes/BitsandBytes.c
+++ Assignment3-Bits and Bytes/BitsandBytes.c
@@ -4,7 +4,7 @@
 
 #include <stdio.h>
 
-/**  bitAnd - x & y using only ~ and |
+/*  bitAnd - x & y using only ~ and |
 *    Example bitAnd(6, 5) = 4
 *    Legal ops:  ~ |  
 *    Max ops: 8 */
@@ -12,12 +12,12 @@ int bitAnd(int x, int y) {
     return ~(~x | ~y);
 }
 
-/**  bitXor - x ^ y using only ~ and |
-/* Example bitXor(4, 5) = 1
+/*  bitXor - x ^ y using only ~ and &
+* Example bitXor(4, 5) = 1
 *  Legal ops:  ~ &  
-*  Max ops: 14 */
+*  Max ops: 14 */ 
 int bitXor(int x, int y) {
-    return ~(x | (~y));
+    return ((~x) & y);
 }
 
 /* sign - return 1 if positive, 0 if zero, and -1 if negative
@@ -25,18 +25,62 @@ int bitXor(int x, int y) {
 * Legal ops: ! ~ & ^ | + << >>
 * Max ops : 10 */
 int sign(int x) {
-    if(x & (~x) == 0) {
-        return 0;
-    }
+    return ((!!x) | (x >> 31));
+}
 
+/* getByte - extract byte n from word x
+* Examples: getByte(0x12345678, 1)  = 0x56
+*Legal ops: ! ~ & ^| + << >>
+Max ops: 6 */
+int getByte(int x, int n) {
+    return 0xff & (x >> (n << 3));
+}
+
+/* logicalShift - shift x to the right by n, using a logical shift
+* can assume that 0 <=n<=31
+* examples: logicalShift(0x87654321, 4) = 0x8765432
+* Legal ops:  ~ & ^ | + << >>
+* Max ops: 20 */
+int logicalShift(int x, int n) {
+    return (x >> n) & (~(((1 << 31) >> n) << 1));
+}
+
+/* conditional - same as x ? y : z
+* example conditional (2, 4, 5) = 4
+* ! ~ & ^ | + << >>
+* Max ops: 16 */
+int conditional(int x, int y, int z) {
+    return z ^ ((y ^ z) & ((!x) + ~0));
+}
+
+/* bang - Compute !x without using !
+*  Examples: bang(3)=0, bang(0)=1
+*  Legal ops: ~ & ^ | + << >>
+*  Max ops: 12 */ 
+int bang(int x) {
+     return (((~x + 1) | x) >> 31) + 1;
+}
+
+ 
+/* Return x with the n bits that begin at position p inverted (i.e. turn 0 /* into 1 and vice versa)
+* and the rest left unchanged. Consider the indices of x to begin with the /* lower -order bit   numbered
+* Legal ops: ~ & ^ | << >>
+* as zero */
+int invert (int x, int p, int n) {
+    return   x ^ (~(~0 << n) << (p + 1 - n));
 }
 
 int main() {
-    int x, y;
-    printf("Enter two numbers: ");
-    scanf("%d %d", &x, &y);
+    int x, y, z;
+    printf("Enter three numbers: ");
+    scanf("%d %d %d", &x, &y, &z);
     printf("BitAnd: %d\n", bitAnd(x, y));
     printf("BitXor: %d\n", bitXor(x, y));
     printf("Sign: %d\n", sign(x));
+    printf("getByte: %d\n", getByte(x, y));
+    printf("logicalShift: %d\n", logicalShift(x, y));
+    printf("conditional: %d\n", conditional(x, y, z));
+    printf("bang: %d\n", bang(x));
+    printf("invert: %d\n", invert(x, y, z));
     return 0;
 }
---
+++ Assignment8-Algorithm/1evenFib.c
@@ -0,0 +1,3 @@
+/**  C program to Print n numbers in Fibonacci series that are even.
+*
+*   @Aparna Goyal, 1911981092, 20/08/2021  */
