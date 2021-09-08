0ea57d142fa1b693f0d03868835f5aaa8de02dfa
Aparna Goyal <aparnagoyal.0003@gmail.com>
2021-09-09 02:03:32 +0530

    Update 1evenFib.c

--- Assignment8-Algorithm/1evenFib.c
+++ Assignment8-Algorithm/1evenFib.c
@@ -4,6 +4,30 @@
 
 #include <stdio.h>
 
+int evenFibonacci(int n);
+
+int evenFibonacci(int n){
+    if(n == 0 || n == 1)
+        return n;
+
+    // Recursive algorithm
+    return evenFibonacci(n - 1) + evenFibonacci(n - 2);
+}
+
 int main() {
+  // The number of even Fibonacci numbers to print:
+  int n;
+  printf("Enter the number of even fibonacci numbers you want to print: ");
+  scanf("%d", &n);
+  int x;
+  for(int i = 0; n > 0; i++){
+    x = evenFibonacci(i);
     
+    // Check if the number returned is even
+    if(x % 2 == 0){
+      printf("%d ", x);
+      n--;
+    }
+  }
+  return 0;
 }
