df4c2f3319b251529d2a16e10ff31c214857047c
Aparna Goyal <aparnagoyal.0003@gmail.com>
2021-09-08 12:15:23 +0530

    Update 4largestRectangleArea.c

--- Assignment4-Stacks-and-Queues/4largestRectangleArea.c
+++ Assignment4-Stacks-and-Queues/4largestRectangleArea.c
@@ -1,3 +1,53 @@
-/**  Problem Link: https://leetcode.com/problems/largest-rectangle-in-histogram/
+/** Problem Link: https://leetcode.com/problems/largest-rectangle-in-histogram/
 *
 *   @Aparna Goyal, 1911981092, 18/08/2021  */
+
+
+
+int largestRectangleArea(int* heights, int heightsSize) {
+    int i, j, bars, area;
+    bool calculated;
+    int maxA = 0;
+    
+    for (i = 0; i < heightsSize; i++) {        
+        bars = 1;
+        calculated = false;
+        
+        j = i - 1;
+        while (j >= 0) {
+            if (heights[j] == heights[i]) {
+                calculated = true;
+                break;
+            } 
+            else if (heights[j] > heights[i]) {
+                bars++;
+                j--;
+            }
+            else {
+                break;
+            }
+        }
+        
+        if (calculated == true) {
+            continue;
+        }
+        
+        j = i + 1;
+        while (j < heightsSize) {
+            if (heights[j++] >= heights[i]) { 
+                bars++;
+            }
+            else {
+                break;
+            }
+        }
+        
+        area = heights[i] * bars;
+        
+        if (area > maxA) { 
+            maxA = area;
+        }
+    }
+    
+    return maxA;
+}
