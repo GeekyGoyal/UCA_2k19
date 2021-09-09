e667c4642c50fc787ffdad38f2ec18781ae5271a
Aparna Goyal <aparnagoyal.0003@gmail.com>
2021-09-09 06:00:34 +0530

    Update 3deQueue.c

---
+++ Assignment6_LinkedLists/3deQueue.c
@@ -0,0 +1,6 @@
+/* C program to Implement a double ended queue (deque) using a Linked List. 
+* 
+* It should perform these operations - add an element at head, add an element at tail, remove element from 
+* head, remove element from tail, peek head, peek tail, check if given element is present in deque or not.
+*
+* @Aparna Goyal, 1911981092, 02/09/2021 */
