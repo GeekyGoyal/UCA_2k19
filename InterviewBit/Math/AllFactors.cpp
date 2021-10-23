59d0a5b051d16b73f8e8acfc951e73db652d5936
Aparna Goyal <aparnagoyal.0003@gmail.com>
2021-10-23 16:41:22 +0530

    Create AllFactors.cpp

---
+++ InterviewBit/Math/AllFactors.cpp
@@ -0,0 +1,15 @@
+/* Problem Link: https://www.interviewbit.com/problems/all-factors/ */
+
+vector<int> Solution::allFactors(int A) {
+    vector<int> ans;
+    for(int i = 1; i <= sqrt(A); i++) {
+        if(A % i == 0) { 
+            ans.push_back(i);
+            if(i != sqrt(A)) {
+                ans.push_back(A/i);
+            }
+        }
+    }
+    sort(ans.begin(), ans.end());
+    return ans;
+}
